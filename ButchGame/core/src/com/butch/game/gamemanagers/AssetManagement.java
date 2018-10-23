89aad57468b2676082677dec9d8ff14f187f26fa
Unknown <chris.webb31@icloud.com>
2018-10-23 12:02:13 +0100

    Added sound files

--- ButchGame/core/src/com/butch/game/gamemanagers/AssetManagement.java
+++ ButchGame/core/src/com/butch/game/gamemanagers/AssetManagement.java
@@ -1,12 +1,14 @@
 package com.butch.game.gamemanagers;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.audio.Music;
 
 public class AssetManagement extends com.badlogic.gdx.assets.AssetManager {
     //TEXTURES
     public final String cowboySprite = "Anim/IdleDown/Idle_1_32x32.png";
     //SOUNDS
-
+    public final Music mainTheme = Gdx.audio.newMusic(Gdx.files.internal("music/Town1.mp3"));
     //PARTICLES
 
     //FONTS

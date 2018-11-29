154781f186b545bd61124be6c96d7c1f206eb57c
Unknown <chris.webb31@icloud.com>
2018-11-29 17:27:04 +0000

    Added Tilesets

---
+++ ButchGame/core/assets/TiledFiles/allTiles.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="allTiles" tilewidth="32" tileheight="32" tilecount="5208" columns="8">
+ <image source="allTiles.png" width="256" height="20832"/>
+ <terraintypes>
+  <terrain name="grass" tile="1257"/>
+ </terraintypes>
+</tileset>

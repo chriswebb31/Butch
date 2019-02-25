3d7b1f29f38531df806b0f114c35e4377bce0700
Unknown <chris.webb31@icloud.com>
2019-02-25 10:07:28 +0000

    Coins now Spin

--- ButchGame/core/src/com/butch/game/gameobjects/abstractinterface/Item.java
+++ ButchGame/core/src/com/butch/game/gameobjects/abstractinterface/Item.java
@@ -8,14 +8,9 @@ import com.badlogic.gdx.math.Vector2;
 import com.butch.game.ButchGame;
 
 public abstract class Item extends ItemPickup {
-    public enum State {ACTIVE}
-    public State currentState = State.ACTIVE;
-    public State previousState = State.ACTIVE;
-    public Animation<TextureRegion> itemAnim;
+
     public int quantity;
     public boolean autoPickup = false;
-    private float stateTimer = 0;
-    private Sprite sprite = new Sprite(ButchGame.assets.get(ButchGame.assets.coinItemSprite, Texture.class));
     public String messageCheck;
 
     public Item(Vector2 position) {
@@ -23,24 +18,6 @@ public abstract class Item extends ItemPickup {
         this.type = 2;
     }
 
-    public void update(float delta) {
-        sprite.setRegion(getFrame(delta));
-        this.setSprite(sprite);
-    }
 
-    public TextureRegion getFrame(float dt) {
-        TextureRegion region = null;
-        currentState = GetState();
-//        System.out.println(this.id);
-//        System.out.println(this.itemAnim.getKeyFrame(stateTimer, true));
-        region = itemAnim.getKeyFrame(stateTimer, true);
-        stateTimer = currentState == previousState ? stateTimer + dt : 0;
-        previousState = currentState;
 
-        return region;
-    }
-
-    private State GetState() {
-        return State.ACTIVE;
-    }
 }
--- ButchGame/core/src/com/butch/game/gameobjects/abstractinterface/ItemPickup.java
+++ ButchGame/core/src/com/butch/game/gameobjects/abstractinterface/ItemPickup.java
@@ -1,19 +1,27 @@
 package com.butch.game.gameobjects.abstractinterface;
 
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.butch.game.ButchGame;
 
 public abstract class ItemPickup extends Renderable{
+    public enum State {ACTIVE}
+    public State currentState = State.ACTIVE;
+    public State previousState = State.ACTIVE;
+    public float stateTimer = 0;
     public int id;
     public Circle collectionRange;
     public boolean colleactable = false;
     public int type;
     public Sound collectionFX;
-
+    public Animation<TextureRegion> itemAnim;
+    public Sprite sprite = new Sprite(ButchGame.assets.get(ButchGame.assets.coinItemSprite, Texture.class));
 
     public ItemPickup(Vector2 position){
         this.TAG = "item";
@@ -26,7 +34,16 @@ public abstract class ItemPickup extends Renderable{
     @Override
     public void update(float delta) {
         if (activeForRender && getSprite() != null) {
-            this.getSprite().setScale(10);
+            if(this.id == 3) {
+                sprite.setRegion(getFrame(delta));
+                this.setSprite(sprite);
+                System.out.println(getFrame(delta));
+                this.getSprite().setScale(4);
+            } else {
+                this.getSprite().setScale(10);
+            }
+            System.out.println(this.id);
+
             this.getSprite().setPosition(this.getPosition().x, this.getPosition().y);
             this.setCollider(new Rectangle(this.getPosition().x, this.getPosition().y, this.getSprite().getWidth() / 3, this.getSprite().getHeight() / 2));
         }
@@ -42,6 +59,26 @@ public abstract class ItemPickup extends Renderable{
         if(this.collectionFX != null){
             collectionFX.play();
         }
+    }
+
+    public TextureRegion getFrame(float dt) {
+        TextureRegion region = null;
+        currentState = GetState();
+//        System.out.println(this.id);
+//        System.out.println(this.itemAnim.getKeyFrame(stateTimer, true));
+        switch(currentState) {
+            case ACTIVE:
+                region = this.itemAnim.getKeyFrame(stateTimer, true);
+                break;
+        }
+        stateTimer = currentState == previousState ? stateTimer + dt : 0;
+        previousState = currentState;
+        System.out.println(region);
+        return region;
+    }
+
+    private State GetState() {
+        return State.ACTIVE;
     }
 
 }

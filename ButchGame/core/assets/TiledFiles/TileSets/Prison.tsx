c0a9a509ec01c2d67f225adcc93bc72d7825d384
TheVivzzy <36890264+TheVivzzy@users.noreply.github.com>
2019-03-02 20:55:17 +0000

    Prison map completed with collisions

---
+++ ButchGame/core/assets/TiledFiles/TileSets/Prison.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="prisonTiles" tilewidth="32" tileheight="32" tilecount="110" columns="10">
+ <image source="../allTiles/prisonTiles.png" width="320" height="352"/>
+</tileset>

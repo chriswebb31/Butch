17e35f1dc00704d857c167e1f9a4b4ee6d2f7bfa
layide <olayidebasorun@gmail.com>
2019-03-07 17:49:58 +0000

    added dialogue classes, not working yet

--- ButchGame/core/src/com/butch/game/screens/GameScreens/NewGameScreen.java
+++ ButchGame/core/src/com/butch/game/screens/GameScreens/NewGameScreen.java
@@ -1,13 +1,28 @@
 package com.butch.game.screens.GameScreens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.butch.game.ButchGame;
 import com.butch.game.screens.GameScreens.Level2;
 import com.butch.game.screens.GameScreens.ModelGameScreen;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Align;
+import com.butch.game.dialouge.DialougeBox;
 
 public class NewGameScreen extends ModelGameScreen {
    public static TiledMap map = ButchGame.assets.get(ButchGame.assets.tilemap1);
+   private int uiScale=2;
+   private Stage uiStage;
+   private Table root;
+   private DialougeBox dialougeBox;
     public NewGameScreen(int levelNumber, ButchGame game, FitViewport gameViewPort, TiledMap map){
 
     super(levelNumber,game,gameViewPort, map, 1);
@@ -26,6 +41,8 @@ public class NewGameScreen extends ModelGameScreen {
             game.setScreen(new Level2(2,game, gameViewPort, player.getGunInventory(), Level2.map, player.getPlayerLevel()));
         }
         super.render(delta);
+        initUI();
+
 //
 //        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
 //        shapeRenderer.setColor(Color.FIREBRICK);
@@ -78,5 +95,17 @@ public class NewGameScreen extends ModelGameScreen {
     @Override
     public void dispose() {
 
+    }
+    private void initUI(){
+     uiStage =new Stage(new ScreenViewport(new OrthographicCamera()));
+     uiStage.getViewport().update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(),true);
+     root = new Table();
+     root.setFillParent(true);
+
+     dialougeBox =new DialougeBox();
+     dialougeBox.animateText("test case example");
+     dialougeBox.setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+     root.add(dialougeBox); //.expand().align(Align.bottom).pad(8f);
+        uiStage.addActor(root);
     }
 }

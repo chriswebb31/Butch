69446c993d6f17c4c1c48e1bcdeb8ad18fd7035a
Unknown <chris.webb31@icloud.com>
2019-03-11 18:16:05 +0000

    Updated Sean's Maps Almost Done

---
+++ ButchGame/core/assets/TiledFiles/TileSets/allTiles-4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="allTiles-4" tilewidth="32" tileheight="32" tilecount="128" columns="8">
+ <image source="../allTiles/allTiles-4.png" width="256" height="512"/>
+</tileset>

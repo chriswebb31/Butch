e561ceb7e0ee4664c1777fda01b9c536b83769dc
TheVivzzy <36890264+TheVivzzy@users.noreply.github.com>
2019-03-14 15:43:31 +0000

    Big City map towards the end completed

---
+++ ButchGame/core/assets/TiledFiles/Houses 4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Houses 4" tilewidth="32" tileheight="32" tilecount="27" columns="9">
+ <image source="allTiles/more_tiles_by_mapxcreations-d93oz32.png" width="295" height="125"/>
+</tileset>
---
+++ ButchGame/core/assets/TiledFiles/villageTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="villageTiles" tilewidth="32" tileheight="32" tilecount="60" columns="10">
+ <image source="allTiles/villageTiles.png" width="320" height="192"/>
+</tileset>

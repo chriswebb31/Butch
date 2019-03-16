a0a1b8724388cb09f9404c070c3afeea752c1c0a
TheVivzzy <36890264+TheVivzzy@users.noreply.github.com>
2019-03-16 14:17:20 +0000

    Ending maze map done (check collisions)

---
+++ ButchGame/core/assets/TiledFiles/Warzone map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Warzone map" tilewidth="32" tileheight="32" tilecount="48" columns="8">
+ <image source="allTiles/warZoneTiles-4.png" width="256" height="192"/>
+</tileset>
---
+++ ButchGame/core/assets/TiledFiles/Warzone.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Warzone" tilewidth="32" tileheight="32" tilecount="144" columns="16">
+ <image source="allTiles/warZoneTiles.png" width="512" height="288"/>
+</tileset>
